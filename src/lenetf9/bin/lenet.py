ff07c4fd3e2ac2b273931fb7cc60a118528e0de4
jgvlf <joaogabrielvlf@gmail.com>
2024-09-30 09:31:01 -0300

    Create and output the LeNet Architecture

--- src/lenetf9/bin/lenet.py
+++ src/lenetf9/bin/lenet.py
@@ -1,3 +1,4 @@
+from lenetf9.arch.lenet.lenet_bin import ArchLeNet
 from lenetf9.bin.args.lenet_args import LenetArgs
 
 
@@ -5,7 +6,7 @@ def main() -> None:
     args = LenetArgs.setup_args()
     if args and args.sub == "info":
         if args.info == "arch":
-            print("ARCH COMMAND INSIDE INFO")
+            ArchLeNet.model_summary()
         else:
             print("INFO COMMAND")
     elif args and args.sub == "train":
